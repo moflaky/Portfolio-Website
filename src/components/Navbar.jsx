ce15d8e04b7a15ad8c53a7084c4aca1cdfec621c
Matthew Salamack <mattymacho3@gmail.com>
2022-04-15 20:30:18 -0400

    linkedin link https

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -96,8 +96,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="www.linkedin.com/in/matthewsalamack
-              "
+              href="https://www.linkedin.com/in/matthewsalamack/"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
